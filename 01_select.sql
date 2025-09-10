0ebdbe47176d4ac98cd29302e51f18b6381b5d17
Paulo Santos <santospaulor21@gmail.com>
2025-09-10 16:12:08 -0300

    feat: select from

--- 01_select.sql
+++ 01_select.sql
@@ -2,4 +2,10 @@ SELECT  c.IdCliente,
         c.QtdePontos, 
         c.DtCriacao 
 FROM clientes c
-LIMIT 10
+LIMIT 10;
+
+SELECT  c.IdCliente, 
+        c.DtCriacao,
+        c.DtAtualizacao 
+FROM clientes c;
+
---
+++ 02_tables.sql
@@ -0,0 +1,5 @@
+-- LIMIT é sempre no final 
+-- e limita as linhas
+SELECT *
+FROM produtos
+LIMIT 5;
